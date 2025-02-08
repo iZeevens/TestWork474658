8531d3bb31c3926fb63d8b8d49c3ede439220a1c
iZeevens <izeevens@gmail.com>
2025-02-08 13:08:11 +0500

    feat: add presist for saving cards

--- weather-app/src/store/weatherStore.ts
+++ weather-app/src/store/weatherStore.ts
@@ -1,24 +1,36 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { ResponseCurrentWeather, ResponseWeatherForecast } from "@/types/types";
 
 interface WeatherState {
   currentWeather: ResponseCurrentWeather | null;
-  forecast: ResponseWeatherForecast[];
+  forecast: ResponseWeatherForecast | null;
   loading: boolean;
   error: string | null;
   setCurrentWeather: (weather: ResponseCurrentWeather | null) => void;
-  setForecast: (forecast: ResponseWeatherForecast[]) => void;
+  setForecast: (forecast: ResponseWeatherForecast | null) => void;
   setLoading: (loading: boolean) => void;
   setError: (error: string | null) => void;
 }
 
-export const useWeatherStore = create<WeatherState>((set) => ({
-  currentWeather: null,
-  forecast: [],
-  loading: false,
-  error: null,
-  setCurrentWeather: (weather) => set({ currentWeather: weather }),
-  setForecast: (forecast) => set({ forecast }),
-  setLoading: (loading) => set({ loading }),
-  setError: (error) => set({ error }),
-}));
+export const useWeatherStore = create<WeatherState>()(
+  persist(
+    (set) => ({
+      currentWeather: null,
+      forecast: null,
+      loading: false,
+      error: null,
+      setCurrentWeather: (weather) => set({ currentWeather: weather }),
+      setForecast: (forecast) => set({ forecast }),
+      setLoading: (loading) => set({ loading }),
+      setError: (error) => set({ error }),
+    }),
+    {
+      name: "weather-storage",
+      partialize: (state) => ({
+        currentWeather: state.currentWeather,
+        forecast: state.forecast,
+      }),
+    }
+  )
+);
